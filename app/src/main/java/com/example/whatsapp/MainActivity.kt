84437ecf6fa4751617514e5ef0f84e2a8d03b346
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-23 13:24:32 +0200

    Setting up the toolbar

--- app/src/main/java/com/example/whatsapp/MainActivity.kt
+++ app/src/main/java/com/example/whatsapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.whatsapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
+    //creating main toolbar
+    private lateinit var mainToolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Setting the toolbar title and its color
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
+        mainToolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(mainToolbar)
+        mainToolbar.title = "WhatsApp"
+        mainToolbar.setTitleTextColor(Color.WHITE)
     }
 }
