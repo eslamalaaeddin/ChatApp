8f1b6cbe97a516202d28809ac5299b30c444affc
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-23 14:10:54 +0200

    adding fragments

---
+++ app/src/main/java/fragments/ChatsFragment.kt
@@ -0,0 +1,21 @@
+package fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.whatsapp.R
+
+class ChatsFragment : Fragment() {
+    private lateinit var generalView: View
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        generalView = View.inflate(context, R.layout.fragment_chats,null)
+        return generalView
+
+    }
+}
