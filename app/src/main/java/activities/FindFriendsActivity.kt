55519701672a408a16a34b95b900c896b0d9951f
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-25 19:14:52 +0200

    adding toolbar

--- app/src/main/java/activities/FindFriendsActivity.kt
+++ app/src/main/java/activities/FindFriendsActivity.kt
@@ -1,12 +1,31 @@
 package activities
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.whatsapp.R
+import com.example.whatsapp.databinding.ActivityFindFriendsBinding
 
 class FindFriendsActivity : AppCompatActivity() {
+    private lateinit var activityFindFriendsBinding: ActivityFindFriendsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_find_friends)
+
+        activityFindFriendsBinding = DataBindingUtil.setContentView(this,R.layout.activity_find_friends)
+
+        setUpToolbar()
+
+
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(activityFindFriendsBinding.mainToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Find friends"
+        activityFindFriendsBinding.mainToolbar.setTitleTextColor(Color.WHITE)
+        activityFindFriendsBinding.mainToolbar.overflowIcon?.setColorFilter(resources.getColor(R.color.white), PorterDuff.Mode.SRC_IN)
     }
 }
