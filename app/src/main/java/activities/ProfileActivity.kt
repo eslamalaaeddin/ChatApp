2276eb8c94fc4d80c9b062b363f33ae8f49f9750
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-25 22:21:50 +0200

    adding profile activity

---
+++ app/src/main/java/activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsapp.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
