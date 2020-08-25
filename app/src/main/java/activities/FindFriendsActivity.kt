d78e64c854902c5df77a74f67bfa99cc761e0e2c
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-25 19:08:10 +0200

    adding find friends activity

---
+++ app/src/main/java/activities/FindFriendsActivity.kt
@@ -0,0 +1,12 @@
+package activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsapp.R
+
+class FindFriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_friends)
+    }
+}
