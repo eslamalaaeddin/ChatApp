661413bf855bce263d32200a71173e3544b38720
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-26 20:46:25 +0200

    adding Utils class

---
+++ app/src/main/java/com/example/whatsapp/Utils.kt
@@ -0,0 +1,17 @@
+package com.example.whatsapp
+/*
+    This util class is for string constants,
+    and repeated functions and declarations
+ */
+object Utils {
+    const val CONTACTS_CHILD = "Contacts"
+    const val GROUPS_CHILD = "Groups"
+    const val USERS_CHILD = "Users"
+    const val CHAT_REQUESTS_CHILD = "Chat requests"
+
+    const val NAME_CHILD = "name"
+    const val IMAGE_CHILD = "image"
+    const val STATUS_CHILD = "status"
+    const val USER_ID_CHILD = "uid"
+
+}
