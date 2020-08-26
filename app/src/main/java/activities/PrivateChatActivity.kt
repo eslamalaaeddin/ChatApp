ccf4c933bf162ea1300e561368115488d51d582b
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-26 21:41:40 +0200

    adding private chat activity

---
+++ app/src/main/java/activities/PrivateChatActivity.kt
@@ -0,0 +1,12 @@
+package activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsapp.R
+
+class PrivateChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_private_chat)
+    }
+}
