882786cddf12ab4ec8ffc815a97f18572c3b4bbb
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-28 06:07:06 +0200

    adding retrofit api interface

---
+++ app/src/main/java/notifications/NotificationApi.kt
@@ -0,0 +1,16 @@
+package notifications
+
+
+import com.example.whatsapp.Utils.CONTENT_TYPE
+import com.example.whatsapp.Utils.SERVER_KEY
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface NotificationApi {
+    @Headers("Authorization: key=$SERVER_KEY", "Content-Type:$CONTENT_TYPE")
+    @POST("fcm/send")
+    suspend fun postNotification(@Body notification: PushNotification): Response<ResponseBody>
+}
