34e24170d19179fab8fb9a25d1ccf31284adb9c8
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-30 07:30:24 +0200

    updating private message model

---
+++ app/src/main/java/com/example/whatsapp/PrivateMessageModel.kt
@@ -0,0 +1,13 @@
+package com.example.whatsapp
+
+class PrivateMessageModel(var from:String,
+                          var message:String,
+                          var type:String,
+                          var to:String,
+                          var messageKey:String,
+                          var date:String,
+                          var time:String,
+                          var fileName:String) {
+
+    constructor() : this ("","","","","","","","")
+}
