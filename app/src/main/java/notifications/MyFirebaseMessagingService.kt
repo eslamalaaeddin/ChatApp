d8c0f787b051ab60ce2b773ed31f8ebfd696c817
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-05 09:05:06 +0200

    adding actions to the notification

--- app/src/main/java/notifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/notifications/MyFirebaseMessagingService.kt
@@ -1,65 +1,98 @@
-package com.example.firebasecloudmessaging
+package notifications
 
-import activities.MainActivity
+import ui.ui.activities.PrivateChatActivity
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.whatsapp.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import ui.ui.activities.VideoChatActivity
 import kotlin.random.Random
 
-
+private const val USER_ID = "user id"
 private const val CHANNEL_ID = "my_channel"
-
+private const val TAG = "MyFirebaseMessagingServ"
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-    companion object {
-        var sharedPref: SharedPreferences? = null
-
-        var token: String?
-            get() {
-                return sharedPref?.getString("token", "")
-            }
-            set(value) {
-                sharedPref?.edit()?.putString("token", value)?.apply()
-            }
-    }
-
-    override fun onNewToken(newToken: String) {
-        super.onNewToken(newToken)
-        token = newToken
-    }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        //video notification
+        if (message.data["message"] == "I Want to make a video chat with you") {
+            //accept inent
+            val acceptVideoChatIntent = Intent(this, VideoChatActivity::class.java)
+            acceptVideoChatIntent.putExtra(USER_ID,message.data["uid"])
 
-        val intent = Intent(this, MainActivity::class.java)
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val notificationID = Random.nextInt()
+            //dismiss intent
+            val dismissVideoChatIntent = Intent(this, PrivateChatActivity::class.java)
+            dismissVideoChatIntent.apply {
+                putExtra(USER_ID,message.data["uid"])
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createNotificationChannel(notificationManager)
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationID = Random.nextInt()
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel(notificationManager)
+            }
+
+            acceptVideoChatIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            dismissVideoChatIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val acceptPendingIntent =
+                PendingIntent.getActivity(this, 0, acceptVideoChatIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val dismissPendingIntent =
+                PendingIntent.getActivity(this, 0, dismissVideoChatIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(message.data["title"])
+                .setContentText(message.data["message"])
+                .setSmallIcon(R.drawable.whatsapp)
+                .setAutoCancel(true)
+                .setContentIntent(acceptPendingIntent)
+                .setContentIntent(dismissPendingIntent)
+                .addAction(R.drawable.tick,"Accept",acceptPendingIntent)
+                .addAction(R.drawable.cross,"Dismiss",dismissPendingIntent)
+                .build()
+
+            notificationManager.notify(notificationID, notification)
+        }
+        //Message notification
+        else{
+            val intent = Intent(this, PrivateChatActivity::class.java)
+            intent.putExtra(USER_ID,message.data["uid"])
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationID = Random.nextInt()
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel(notificationManager)
+            }
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(message.data["title"])
+                .setContentText(message.data["message"])
+                .setSmallIcon(R.drawable.whatsapp)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build()
+
+            notificationManager.notify(notificationID, notification)
         }
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(message.data["title"])
-            .setContentText(message.data["message"])
-            .setSmallIcon(R.drawable.ic_android_black_24dp)
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-            .build()
 
-        notificationManager.notify(notificationID, notification)
+
+//        showBackgroundNotification(0,notification)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -71,6 +104,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             lightColor = Color.GREEN
         }
         notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun showBackgroundNotification(
+        requestCode: Int,
+        notification: Notification
+    ) {
+        val intent = Intent(PrivateChatActivity.ACTION_SHOW_NOTIFICATION).apply {
+            putExtra(PrivateChatActivity.REQUEST_CODE, requestCode)
+            putExtra(PrivateChatActivity.NOTIFICATION, notification)
+        }
+        sendOrderedBroadcast(intent, PrivateChatActivity.PERM_PRIVATE)
     }
 
 }
