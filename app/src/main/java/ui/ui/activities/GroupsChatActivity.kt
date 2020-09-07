5841b11799e37a4fb0b7955ec73e5985cfd2dc4a
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-07 10:28:22 +0200

    adding groups chat activity and layout

---
+++ app/src/main/java/ui/ui/activities/GroupsChatActivity.kt
@@ -0,0 +1,12 @@
+package ui.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsapp.R
+
+class GroupsChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_groups_chat)
+    }
+}
