586ed524ad7fa47389196fe2a4493dc9b8bee605
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-08 10:41:16 +0200

    retrieving statuses

--- app/src/main/java/ui/ui/fragments/StatusFragment.kt
+++ app/src/main/java/ui/ui/fragments/StatusFragment.kt
@@ -1,18 +1,23 @@
 package ui.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.whatsapp.R
+import com.example.whatsapp.Utils
 import com.example.whatsapp.databinding.FragmentChatsBinding
 import com.example.whatsapp.databinding.FragmentStatusBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import models.CallModel
+import models.StatusModel
+import ui.ui.activities.StatusActivity
 
 class StatusFragment : Fragment() {
     private lateinit var fragmentStatusBinding : FragmentStatusBinding
@@ -22,7 +27,7 @@ class StatusFragment : Fragment() {
     private lateinit var currentUserId: String
     private lateinit var usersReference: DatabaseReference
     private lateinit var rootReference: DatabaseReference
-
+    private var statusList = mutableListOf<StatusModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,6 +40,12 @@ class StatusFragment : Fragment() {
         contactsReference = FirebaseDatabase.getInstance().reference.child("Contacts").child(currentUser.uid)
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        retrieveStatuses()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -49,5 +60,38 @@ class StatusFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fragmentStatusBinding.fab.setOnClickListener {
+            startActivity(Intent(context,StatusActivity::class.java))
+        }
+
+    }
+
+    private fun retrieveStatuses() {
+        rootReference.child(Utils.USERS_CHILD).child(currentUserId).child("Status").addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                statusList.clear()
+                for (status in snapshot.children) {
+
+                    val text = status.child("text").value.toString()
+                    val color = status.child("color").value.toString()
+                    val viewersId = status.child("viewersid").value.toString()
+                    val viewsCount = status.child("viewscount").value.toString()
+                    val statusId = status.child("statusid").value.toString()
+                    val date = status.child("date").value.toString()
+                    val time = status.child("time").value.toString()
+
+                    val currentStatus = StatusModel(text,color,viewersId,viewsCount,statusId,date,time)
+
+                    statusList.add(0, currentStatus)
+                }
+//                callsAdapter = CallsAdapter(statusList)
+//                fragmentCallsBinding.callsRecyclerView.adapter = callsAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
