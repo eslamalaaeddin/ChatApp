f279c191811d889676aac2a96d75b924b58fe9fe
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-08 09:00:28 +0200

    updating status activity logic

--- app/src/main/java/ui/ui/activities/StatusActivity.kt
+++ app/src/main/java/ui/ui/activities/StatusActivity.kt
@@ -1,12 +1,34 @@
 package ui.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.whatsapp.R
+import com.example.whatsapp.Utils
+import com.example.whatsapp.databinding.ActivityStatusBinding
+
 
 class StatusActivity : AppCompatActivity() {
+    private lateinit var statusBinding: ActivityStatusBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_status)
+
+        statusBinding = DataBindingUtil.setContentView(this, R.layout.activity_status)
+
+        statusBinding.paletteImageView.setOnClickListener {
+            val random = (0..7).random()
+            statusBinding.statusEditText.setBackgroundColor(resources.getColor(Utils.COLORS[random]))
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val window: Window = window
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.statusBarColor = resources.getColor(Utils.COLORS[random])
+            }
+        }
+
     }
 }
