f4ad8e3f130ee9463483c41e8d3a97415f893f29
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-08 07:19:07 +0200

    adding data binding

--- app/src/main/java/ui/ui/fragments/StatusFragment.kt
+++ app/src/main/java/ui/ui/fragments/StatusFragment.kt
@@ -4,17 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.whatsapp.R
+import com.example.whatsapp.databinding.FragmentChatsBinding
+import com.example.whatsapp.databinding.FragmentStatusBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class StatusFragment : Fragment() {
-    private lateinit var generalView: View
+    private lateinit var fragmentStatusBinding : FragmentStatusBinding
+    private lateinit var contactsReference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var currentUserId: String
+    private lateinit var usersReference: DatabaseReference
+    private lateinit var rootReference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        currentUser = auth.currentUser!!
+
+        rootReference = FirebaseDatabase.getInstance().reference
+        currentUserId = currentUser.uid
+        usersReference = FirebaseDatabase.getInstance().reference.child("Users")
+        contactsReference = FirebaseDatabase.getInstance().reference.child("Contacts").child(currentUser.uid)
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        generalView = View.inflate(context , R.layout.fragment_status , null)
-        return generalView
+        fragmentStatusBinding =
+            DataBindingUtil.inflate(inflater , R.layout.fragment_status , container,false)
+
+        return fragmentStatusBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
