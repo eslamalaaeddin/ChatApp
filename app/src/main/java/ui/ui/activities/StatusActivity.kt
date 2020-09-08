6d8a502edef6290c598524c7dd47902fa5c618fd
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-08 10:30:15 +0200

    completing status activity logic

--- app/src/main/java/ui/ui/activities/StatusActivity.kt
+++ app/src/main/java/ui/ui/activities/StatusActivity.kt
@@ -1,33 +1,117 @@
 package ui.ui.activities
 
-import android.graphics.Color
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.whatsapp.R
 import com.example.whatsapp.Utils
+import com.example.whatsapp.Utils.USERS_CHILD
 import com.example.whatsapp.databinding.ActivityStatusBinding
+import com.example.whatsapp.databinding.FragmentStatusBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class StatusActivity : AppCompatActivity() {
     private lateinit var statusBinding: ActivityStatusBinding
+
+    private lateinit var fragmentStatusBinding : FragmentStatusBinding
+    private lateinit var contactsReference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var currentUserId: String
+    private lateinit var usersReference: DatabaseReference
+    private lateinit var rootReference: DatabaseReference
+
+    private var statusColor:Int = 0
+    @SuppressLint("SimpleDateFormat", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+        currentUser = auth.currentUser!!
+
+        rootReference = FirebaseDatabase.getInstance().reference
+        currentUserId = currentUser.uid
+        usersReference = FirebaseDatabase.getInstance().reference.child("Users")
+        contactsReference = FirebaseDatabase.getInstance().reference.child("Contacts").child(currentUser.uid)
+
         statusBinding = DataBindingUtil.setContentView(this, R.layout.activity_status)
+
+        statusColor = resources.getColor(R.color.colorPrimary)
+        val calender = Calendar.getInstance()
+        //get date and time
+        val dateFormat = SimpleDateFormat("MMM dd, yyyy")
+        val timeFormat = SimpleDateFormat("hh:mm a")
 
         statusBinding.paletteImageView.setOnClickListener {
             val random = (0..7).random()
             statusBinding.statusEditText.setBackgroundColor(resources.getColor(Utils.COLORS[random]))
-
+            statusColor = resources.getColor(Utils.COLORS[random])
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 val window: Window = window
                 window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
                 window.statusBarColor = resources.getColor(Utils.COLORS[random])
             }
+        }
+
+        //handling the visibility of fab
+        statusBinding.statusEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(text: Editable?) {
+                if (text.toString().isEmpty()){
+                    statusBinding.fab.visibility = View.INVISIBLE
+                }else{
+                    statusBinding.fab.visibility = View.VISIBLE
+                }
+            }
+        })
+
+        //adding status logic
+        statusBinding.fab.setOnClickListener {
+          val statusKey =  rootReference.child(USERS_CHILD).child("Status").push().key.toString()
+
+
+           val currentDate = dateFormat.format(calender.time)
+           val currentTime = timeFormat.format(calender.time)
+
+            val statusText  = statusBinding.statusEditText.editableText.toString()
+
+            val statusMap = HashMap<String, Any>()
+            statusMap["text"] = statusText
+            statusMap["statuscolor"] = statusColor.toString()
+            statusMap["viewersid"] = "01021823908"
+            statusMap["viewscount"] = "15"
+            statusMap["statusid"] = statusKey
+            statusMap["date"] = currentDate
+            statusMap["time"] = currentTime
+
+            rootReference.child(USERS_CHILD).child(currentUserId).child("Status").child(statusKey).
+            updateChildren(statusMap).addOnCompleteListener {
+                Toast.makeText(this, "Status Added", Toast.LENGTH_SHORT).show()
+            }
+
+
+
         }
 
     }
